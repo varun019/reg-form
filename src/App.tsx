c2eed1251372b3657e59b5612efcb829b1bd7620
varun019 <varunsaraswat05@gmail.com>
2023-06-13 12:30:04 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import FormData from "./components/FormData";
+import './App.css'; 
+
+const App = () =>{
+  return(
+    <>
+    <h3 className="mt-4 mb-5 text-center"> Registration Form</h3>
+
+    <FormData/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/FormData.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useForm } from 'react-hook-form';
+
+type Formdata = {
+    name: string,
+    email: string,
+    password: string
+}
+
+const FormData: React.FC = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<Formdata>();
+
+    const onSubmit = (data: Formdata) => {
+        console.log(data);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="container mt-5 box">
+                <div className="input-group mb-3">
+                    <label htmlFor="fname" className="fw-bold fs-5">Name:</label>
+                    <input {...register('name', { required: 'Name is Required' ,minLength:5,maxLength:15})} type="text" name='name' className="name" placeholder="Enter your Name" aria-label="Username" aria-describedby="basic-addon1" /><br />
+                    {errors.name && <span>{errors.name.message}</span>}
+                </div>
+
+                <label htmlFor="email" className="fw-bold fs-5">Email:</label>
+                <input type="email" id='email' {...register('email', { required: 'Email is Required' })} onChange={(e) => e.target.value} placeholder="Enter your Email" className="" /><br />
+                {errors.email && <span>{errors.email.message}</span>}<br/>
+
+                <label htmlFor="password" className="fw-bold fs-5 mt-2">Password:</label>
+                <input type="password" id='password' {...register('password', { required: 'Password is Required' })} onChange={(e) => e.target.value} placeholder="Enter your Password" className="mb-4" />
+                {errors.password && <span>{errors.password.message}</span>}
+
+                <div className="d-flex text-start">
+                    <button className="btn btn-primary fs-5 w-100 mt-4">Register</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default FormData;
